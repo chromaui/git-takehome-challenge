d96dd13c4ef5cd4df6331b4ddec47e53664dcaa9
Vanessa Yuen <6842965+vanessayuenn@users.noreply.github.com>
2025-10-16 13:48:20 +0200

    upgrade to SB 9

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,18 +3,17 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   staticDirs: ["../public"],
+
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@storybook/addon-designs",
     "@chromatic-com/storybook",
+    "@storybook/addon-docs"
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+  }
 };
 export default config;
